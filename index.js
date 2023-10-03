4ef3d0cd8ff1554e3333b3a049b13b2c60e3ba67
mosesmramba <moses.mramba@student.moringaschool.com>
2023-10-03 15:01:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+const formData = {
+      name: name,
+      email: email
+};
+const configObj = {
+     method: "POST",
+     headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+ },
+  body: JSON.stringify(formData)
+};
+fetch("http://localhost:3000/users", configObj)
+.then(function(response) {
+return response.json();
+})
+      .then(function(object) {
+          const h2 = document.createElement('h2');
+          h2.innerHTML = object.id;
+          document.body.appendChild(h2);
+          console.log(object);
+      })
+      .catch(function(error) {
+          const h3 = document.createElement('h3');
+          h3.innerHTML = error.message;
+          document.body.appendChild(h3);
+          console.log(error.message);
+      });
+}
